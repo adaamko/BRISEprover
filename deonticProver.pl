bcca6967fd125a0957af21d9f84e6688380b6e65
blellmann <lellmann@logic.at>
2021-02-22 20:51:41 +0100

    added option for including the standard deontic operators

--- deonticProver.pl
+++ deonticProver.pl
@@ -131,13 +131,19 @@ prove_online(Fml, Facts, D_Assumptions, Sup_Relation, Operators,
     \+ member(test,Facts),
     prove_with_filename(Fml, Operators, Inclusions, Conflicts, P_list,
 			Facts, D_Assumptions, Sup_Relation, Version, Filename).
+% For adding the standard operators
+prove_online(Fml, Facts, D_Assumptions, Sup_Relation, Operators,
+	     Inclusions, Conflicts, P_list, [standardoperators], Version, derivability, Filename) :-
+    prove_with_filename(Fml, [(obl,obl), (for,for), (per,obl)|Operators], Inclusions, [confl(obl,obl), confl(obl,per), confl(obl,for), confl(for,for), confl(for,per)|Conflicts], P_list,
+			Facts, D_Assumptions, Sup_Relation, Version, Filename).
 % for testing with the plandokumente
 prove_online(Fml, Facts, D_Assumptions, Sup_Relation, Operators,
 	     Inclusions, Conflicts, P_list, Ex_list, Version, derivability, Filename) :-
-    phrase(added_facts(Facts,Ex_list),New_Facts),
+    subtract(Ex_list,[standardoperators],Ex_list1),
+    phrase(added_facts(Facts,Ex_list1),New_Facts),
 %    facts(plangebiet(7602),L),
 %    append(Facts,L,Facts1),
-    phrase(added_assumptions(D_Assumptions,Ex_list),New_D_Assumptions1),
+    phrase(added_assumptions(D_Assumptions,Ex_list1),New_D_Assumptions1),
 %    obligations_plangebiet(plangebiet(7602),O),
 %    append(O,D_Assumptions,D_Assumptions1),
     phrase(bauordnung(b),Bauordnung_assumptions),
@@ -175,10 +181,11 @@ prove_online(Fml, Facts, D_Assumptions, Sup_Relation, Operators,
 */
 explain_online(Fml, Facts, D_Assumptions, Sup_Relation, Operators,
 	     Inclusions, Conflicts, P_list, Ex_list, Version, derivability, Filename) :-
-    phrase(added_facts(Facts,Ex_list),New_Facts),
+    subtract(Ex_list,[standardoperators],Ex_list1),
+    phrase(added_facts(Facts,Ex_list1),New_Facts),
 %    facts(plangebiet(7602),L),
 %    append(Facts,L,Facts1),
-    phrase(added_assumptions(D_Assumptions,Ex_list),New_D_Assumptions1),
+    phrase(added_assumptions(D_Assumptions,Ex_list1),New_D_Assumptions1),
 %    obligations_plangebiet(plangebiet(7602),O),
 %    append(O,D_Assumptions,D_Assumptions1),
     phrase(bauordnung(b),Bauordnung_assumptions),
