09f8959732d3ed7aed33ed15d65371038c7836a0
blellmann <lellmann@logic.at>
2021-04-29 22:50:28 +0200

    updated license

--- interpretations.pl
+++ interpretations.pl
@@ -1,21 +1,3 @@
-/*
-Copyright 2019 Bjoern Lellmann
-
-    This file is part of deonticProver 1.2.
-
-    deonticProver 1.2 is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-    deonticProver 1.2 is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with deonticProver 1.2.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 /* 
    For finding alternative interpretations and scoring them.
    So far the implemented alternatives are:
